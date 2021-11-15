1a60697e7638ea121c531de4d4481813d6593612
EthanG78 <ethangarnier78@gmail.com>
2021-11-15 18:56:28 -0400

    Removed form and added dropdown for user to select cohort/semester

--- src/counts.js
+++ src/counts.js
@@ -5,7 +5,6 @@ import {
   Dropdown,
   Table,
   Form,
-  Button,
   Row,
   Col
 } from "react-bootstrap";
@@ -14,8 +13,21 @@ import axios from "axios";
 
 import "./styles.css";
 
-export default function Counts() {
-  const [value, setValue] = useState({ title: "Cohort", input: "2021-01-01" });
+export default function App() {
+  // State variable that represents the list of
+  // react-bootstrap dropdown items currently being displayed in
+  // the count selector dropdown
+  const [menu, setMenu] = useState([]);
+  // State variable that is populated only once on page render.
+  // Holds two lists of react-bootstrap dropdown items, one for
+  // all cohorts being stored in the db and one for all semesters
+  const [items, setItems] = useState({
+    cohorts: [],
+    semesters: []
+  });
+  // State variable that represents the current parameter choice
+  const [value, setValue] = useState({ title: "", input: "" });
+  // State variable that represents the counts being displayed to the user
   const [counts, setCounts] = useState({
     coop: 0,
     total: 0,
@@ -25,6 +37,8 @@ export default function Counts() {
     SEN: 0
   });
 
+  // Effect that makes appropriate API call based on the chosen parameter
+  // and populates the counts state variable for display to user
   useEffect(() => {
     const regexArr = [/\d{4}-\d{2}-\d{2}$/, /\d{4}\/FA|WI|SM$/];
     let url = "";
@@ -53,6 +67,36 @@ export default function Counts() {
     }
   }, [value]);
 
+  // Effect that is run once on page render. Makes an API call which retrieves every unqiue
+  // cohort (start date) and semester currently being stored in the database. Stores
+  // these values as react-bootstrap dropdown items in the items state variable
+  useEffect(() => {
+    axios.get("http://127.0.0.1:8000/api/count_parameters").then((res) => {
+      let cohortItems = [];
+      for (const cohort of res.data.cohorts) {
+        cohortItems.push(
+          <Dropdown.Item eventKey={cohort}>{cohort}</Dropdown.Item>
+        );
+      }
+
+      let semesterItems = [];
+      for (const semester of res.data.semesters) {
+        semesterItems.push(
+          <Dropdown.Item eventKey={semester}>{semester}</Dropdown.Item>
+        );
+      }
+
+      setItems({
+        cohorts: cohortItems,
+        semesters: semesterItems
+      });
+
+      setValue({ title: "Cohort", input: res.data.cohorts[0] });
+
+      setMenu(cohortItems);
+    });
+  }, []);
+
   return (
     <div className="countsCard">
       <div className="headerDiv">
@@ -61,7 +105,14 @@ export default function Counts() {
             id="dropdown-basic-button"
             title={value.title}
             variant="danger"
-            onSelect={(e) => setValue({ title: e, input: value.input })}
+            onSelect={(e) => {
+              setValue({ title: e, input: value.input });
+              if (e.toLowerCase() === "cohort") {
+                setMenu(items.cohorts);
+              } else {
+                setMenu(items.semesters);
+              }
+            }}
           >
             <Dropdown.Item eventKey="Cohort">Cohort</Dropdown.Item>
             <Dropdown.Item eventKey="Semester">Semester</Dropdown.Item>
@@ -71,21 +122,19 @@ export default function Counts() {
           <Form>
             <Row>
               <Col>
-                <input 
-                 
-                  className="inputValueForm"
-                  type="text"
-                  placeholder={value.input}
-                  onChange={(e) =>
-                    setValue({ title: value.title, input: e.target.value })
-                  }
-                />
+                <DropdownButton
+                  id="dropdown-basic-button"
+                  title={value.input}
+                  variant="danger"
+                  onSelect={(e) => setValue({ title: value.title, input: e })}
+                >
+                  {menu}
+                </DropdownButton>
               </Col>
             </Row>
           </Form>
         </div>
       </div>
-      <br />
       <div className="tableDiv">
         <Table hover size="sm">
           <thead>
